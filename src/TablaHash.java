929f431f02ab5a15575c922af3423fcfca424ea8
fujonanda <323145673@ciencias.unam.mx>
2026-09-08 23:24:14 -0600

    feat: implement insert operation

--- src/TablaHash.java
+++ src/TablaHash.java
@@ -26,4 +26,33 @@ public class TablaHash<V> {
     public double factorCarga() {
         return (double) numElementos / tabla.length;
     }
+
+    public void insertar(int key, V value) {
+            if (key < 0) {
+            System.out.println("No se permite insertar llaves negativas");
+            return;
+        }
+        int indice = hash(key);
+        Nodo<V> actual = tabla[indice];
+        Nodo<V> previo = null;
+        while (actual != null && actual.key < key) {
+            previo = actual;
+            actual = actual.siguiente;
+        }
+        if (actual != null && actual.key == key) {
+            actual.value = value;
+            System.out.println("Valor actualizado de " + key + " -> " + value);
+            return;
+        }
+        Nodo<V> nuevoNodo = new Nodo<>(key, value);
+        if (previo == null) {
+            nuevoNodo.siguiente = tabla[indice];
+            tabla[indice] = nuevoNodo;
+        } else {
+            nuevoNodo.siguiente = actual;
+            previo.siguiente = nuevoNodo;
+        }
+        numElementos++;
+        System.out.println("Llave " + key + " insertada en el índice " + indice);
+    }
 }
